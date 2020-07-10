1e3c975a24cd876caf831129c97cdd86ec8756d2
gigibean <gigibean@naver.com>
2020-07-10 18:49:02 +0900

    Update example_1.js

--- React/example_1.js
+++ React/example_1.js
@@ -11,16 +11,16 @@ plusTwo_1 = (s) => {
 
 console.log(plusTwo_1(10)); //12
 
-pluseTwo_2 = s => s+2;
+pluseTwo_2 = s => s + 2;
 
 console.log(pluseTwo_2(10)); //12
 
 //Map 
-arr = [1,2,3,4,5,7];
+arr = [1, 2, 3, 4, 5, 7];
 
 // arr_map = arr.map(function(value) {return value * 2;}) //ES5
 
-arr_map = arr.map(value => value*2);
+arr_map = arr.map(value => value * 2);
 
 console.log(arr_map);
 
@@ -61,7 +61,7 @@ exam_1.getAge(); //23
 class Person {
     constructor(age) {
         this.age = age;
-        
+
     }
     printInfo() {
         console.log("I'm " + this.name + "and " + this.age);
@@ -81,3 +81,77 @@ class User extends Person {
 exam_1 = new User("John", 23);
 exam_1.getName(); // I'm John
 exam_1.printInfo(); //I'm Johnand 23
+
+// 비동기성 동기성
+setTimeout(() => {
+    console.log("hello wolrd")
+}, 2000)
+console.log("bye world");
+
+/*
+bye world
+hello wolrd
+*/
+
+function greeting(sayGoodbye) {
+    setTimeout(() => {
+        console.log("sayHello");
+        sayGoodbye();
+    }, 2000);
+}
+greeting(()=>console.log("bye worold"))
+
+/*
+sayHello
+bye worold
+*/
+
+// 비동기 1
+function greeting(name, callback) {
+    setTimeout(() => {
+        console.log(name + " 님 안녕하세요!");
+        callback(name);
+    }, 2000);
+}
+
+greeting("김길동", function (name) {
+    console.log(name + " 님 안녕히 가세요!");
+})
+
+// 비동기 2
+function greeting(name, callback) {
+    console.log(name + " 님 안녕하세요!");
+    callback(name);
+}
+
+greeting("김길동", function (name) {
+    console.log(name + " 님 안녕히 가세요!");
+})
+
+// Promise
+
+function sayHello2(name) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            console.log(name + "님 안녕하세요");
+            resolve("서울");
+        }, 3000)
+    }
+    )
+}
+
+sayHello2("John").then((location)=> console.log(location + "로 안녕히가세요")); 
+//John님 안녕하세요 서울로 안녕히 가세요
+
+/*
+John님 안녕하세요
+서울로 안녕히가세요
+*/
+
+async function sayHelloBye(name) {
+    loc = sayHello2(name)
+
+    console.log(loc + "로 안녕히 가세요");
+}
+
+sayHelloBye("John") //[object Promise]로 안녕히 가세요 John님 안녕하세요
