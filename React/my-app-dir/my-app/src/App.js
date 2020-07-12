ec604d2729bb9484fca92f85679ee1517628adcc
gigibean <gigibean@naver.com>
2020-07-12 20:15:18 +0900

    Update App.js

--- React/my-app-dir/my-app/src/App.js
+++ React/my-app-dir/my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function worldClock(props) {
+function WorldClock(props) {
   return (
     <div className={'worldClock'}>
         <h2>🌏 {props.city}</h2>
@@ -16,10 +16,10 @@ function App() {
     <div className="App">
       <h1 className={'myClass'}>Hello world</h1>
       <p>this is an example react app :)</p>
-      <worldClock city={'서울'} time={10}/>
-      <worldClock city={'베이징'} time={9}/>
-      <worldClock city={'시드니'} time={12}/>
-      <worldClock city={'LA'} time={17}/>
+      <WorldClock city={'서울'} time={10}/>
+      <WorldClock city={'베이징'} time={9}/>
+      <WorldClock city={'시드니'} time={12}/>
+      <WorldClock city={'LA'} time={17}/>
 
     </div>
   );
