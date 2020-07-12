34e117d7a83619ea20da9dc6ad7acaf4d344ff9b
gigibean <gigibean@naver.com>
2020-07-12 20:05:59 +0900

    Update App.js
    doc4.md

--- React/my-app-dir/my-app/src/App.js
+++ React/my-app-dir/my-app/src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+function worldClock(props) {
+  return (
+    <div className={'worldClock'}>
+        <h2>🌏 {props.city}</h2>
+        <p>⏰ {props.time} 시</p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <h1 className={'myClass'}>Hello world</h1>
+      <h1 classN ame={'myClass'}>Hello world</h1>
       <p>this is an example react app :)</p>
+      <worldClock city={'서울'} time={10}/>
+      <worldClock city={'베이징'} time={9}/>
+      <worldClock city={'시드니'} time={12}/>
+      <worldClock city={'LA'} time={17}/>
+
     </div>
   );
 }
