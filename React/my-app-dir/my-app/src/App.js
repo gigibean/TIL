7ef25bec41bff2dbcb8102863e2be2b403397843
gigibean <gigibean@naver.com>
2020-07-13 11:29:02 +0900

    Update App.js

--- React/my-app-dir/my-app/src/App.js
+++ React/my-app-dir/my-app/src/App.js
@@ -2,23 +2,38 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const cityTimeData = [
-    ['서울', 10],
-    ['베이징', 9],
-    ['시드니', 12],
-    ['시드니', 17],
-  ]
-  const worldClockList = cityTimeData.map((props, index)=>
-    <WorldClock city={props[0]} time={props[1]} key={index}/>
-  )
-  return (
-    <div className="App">
-      <h1 className={'myClass'}>Hello world</h1>
-      <p>this is an example react app :)</p>
-      {worldClockList}
-    </div>
-  );
+class  App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.cityTimeData = [
+      ['서울', 10],
+      ['베이징', 9],
+      ['시드니', 12],
+      ['시드니', 17],
+    ]
+    this.state = {
+      content: ""
+    }
+  }
+
+  handlingChange = (event) => {
+    this.setState({content: event.target.value})
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1 className={'myClass'}>Hello world</h1>
+        <p>this is an example react app :)</p>
+        <div className={'post'}>
+          <textarea value={this.state.content} onChange={this.handlingChange}></textarea>
+        </div>
+        {this.cityTimeData.map((props, index)=>
+          <WorldClock city={props[0]} time={props[1]} key={index}/>
+        )}
+      </div>
+    );
+  }
 }
 
 class WorldClock extends React.Component {
@@ -26,9 +41,10 @@ class WorldClock extends React.Component {
     super(props)
     this.state = {
       hour: this.props.time,
-      minute: 0
+      minute: 0,
+      stop: false,
     }
-    setInterval(()=>{
+    this.timer = setInterval(()=>{
       this.setState((state) => (
       state.minute === 59
         ?{hour: state.hour + 1, minute: 0}
@@ -36,12 +52,19 @@ class WorldClock extends React.Component {
       )
     }, 1000)
   }
+
+  handlingClick = (event) => {
+    this.setState({stop : event.target.value})
+    clearInterval(this.timer)
+  }
+
   // 미리 약속된 함수
   render() {
     return (
       <div className={'worldClock'}>
           <h2>🌏 {this.props.city}</h2>
           <p>⏰ {this.state.hour} 시 {this.state.minute} 분</p>
+          <button value={true} onClick={this.handlingClick}>stop</button>
       </div>
     )
   }
