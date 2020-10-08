38243fff99c716ecf54272be4f6e3f2d10cba108
gigibean <gigibean@naver.com>
2020-10-08 17:42:47 +0900

    Update array 3

--- React/example/begin-react/src/UserList.js
+++ React/example/begin-react/src/UserList.js
@@ -30,9 +30,11 @@ function UserList() {
 
     return (
         <div>
-            <User user={users[0]} />
-            <User user={users[1]} />
-            <User user={users[2]} />
+            {
+                users.map(
+                    user => (<User user={user} key={user.id}/>)
+                )
+            }
         </div>
     );
 }
