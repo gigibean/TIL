7a074094d5a30998118bd77316a00dbeaa6e0f0f
gigibean <gigibean@naver.com>
2020-10-08 16:42:52 +0900

    Update useref

--- React/example/begin-react/src/InputSample.js
+++ React/example/begin-react/src/InputSample.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useRef } from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
         name: '',
         nickname: '',
     });
+    const nameInput = useRef();
     const {name, nickname} = inputs;
     const onChange = (e) => {
         // console.log(e.target.name);
@@ -26,11 +27,14 @@ function InputSample() {
         setInputs({
             name: '',
             nickname: '',
-        })
-    }
+        });
+        nameInput.current.focus();
+    };
     return (
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="name" placeholder="이름" onChange={onChange} value={name}
+            ref={nameInput} 
+            />
             <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
             <button onClick={onRest}>초기화</button>
             <div>
