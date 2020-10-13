c8c8f01a4a76acf79cc7ea936a0c0698cf3dfc28
gigibean <gigibean@naver.com>
2020-10-13 14:50:37 +0900

    Update useReducer

--- React/example/begin-react/src/App.js
+++ React/example/begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useMemo, useCallback} from 'react';
+import React, {useRef, useReducer, useMemo, useCallback} from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -7,23 +7,12 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
+const initialState = {
+  inputs: {
+    useranme: '',
     email: '',
-  });
-  const { username, email } = inputs;
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value
-      });
-    },
-    [inputs]
-  );
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: 'Kim',
@@ -42,49 +31,87 @@ function App() {
       email: 'park@gmail.com',
       active: false
     }
-  ]); 
-  const nextId = useRef(4);
-  // 이 값이 바뀐다고 리렌더링 될 필요 없기 때문에 useState가 아닌 useRef 를 사용
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users => users.concat(user));
+  ]
+}
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name] : action.value
+        }
+      };
+      case 'CREATE_USER':
+        return {
+          inputs: initialState.inputs,
+          users: state.users.concat(action.user)
+        }
+      case 'TOGGLE_USER':
+        return {
+          ...state,
+          users: state.users.map(user =>
+            user.id === action.id ? {...user, active: !user.active} : user)
+        };
+      case 'REMOVE_USER':
+        return {
+          ...state,
+          users: state.users.filter(user => user.id !== action.id)
+        }
+      default:
+        throw new Error('Unhandled action');
+  }
+}
 
-    setInputs({
-      username: '',
-      email: ''
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const nextId = useRef(4);
+  const {users} = state;
+  const {username, email} = state.inputs;
+
+const onChange = useCallback(e => {
+  const {name, value} = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  }, [])
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
     });
     nextId.current += 1;
   }, [username, email]);
 
-  const onRemove = useCallback(
-    id => {
-      setUsers(users => users.filter(user => user.id !== id));
-    },
-    []
-  );
-  const onToggle = useCallback(
-    id => {
-      setUsers(
-        users => 
-        users.map(user =>
-          user.id === id ? { ...user, active: !user.active } : user
-        )
-      );
-    },
-    []
-  );
-  //const count = countActiveUsers(users);
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
+
+
+  const onRemove = useCallback(id =>{
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
-      <CreateUser username={username}  email={email} onChange={onChange} onCreate={onCreate} />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
-      <div><p>사용자 활성화 수</p>{count}</div>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+      <div><p>사용자 활성화 수: {count}</p></div>
     </>
   );
 }
--- React/example/begin-react/src/index.js
+++ React/example/begin-react/src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Counter from './Counter';
 
-// ReactDOM.render(
-//   // page에서 root인 것을 찾아서 <App />을 넣겠다.
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root') 
-//   // in public/index.html
-// );
 ReactDOM.render(
-  <Counter />, document.getElementById('root')
+  // page에서 root인 것을 찾아서 <App />을 넣겠다.
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') 
+  // in public/index.html
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
