2240588b1fd15d46ad8d88842046f1a8a4bc195f
gigibean <gigibean@naver.com>
2020-10-13 13:49:25 +0900

    Update useReducer

--- React/example/begin-react/src/Counter.js
+++ React/example/begin-react/src/Counter.js
@@ -1,32 +1,59 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
+
+function reducer(state, action) {
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
 
 function Counter() {
-    const [number, setNumber] = useState(0);
-
+    const [number, dispatch] = useReducer(reducer, 0);
     const onIncrease = () => {
-        if(number < 10) {
-            setNumber(prevNumber => prevNumber + 1)
-        }
-        else {
-            alert('10 is limit')
-        }
-
+        dispatch({type: 'INCREMENT'});
     }
     const onDecrease = () => {
-        if(number > 0) {
-            setNumber(prevNumber => prevNumber - 1)
-        }
-        else {
-            alert('0 is limit')
-        }
+        dispatch({type: 'DECREMENT'});
     }
     return (
-        <div>
-            <h1 id="number">{number}</h1>
-            <button onClick={onIncrease}>+1</button>
-            <button onClick={onDecrease}>-1</button>
-        </div>
+        <>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}> + 1 </button>
+            <button onClick={onDecrease}> - 1 </button>
+        </>
     )
 }
+// function Counter() {
+//     const [number, setNumber] = useState(0);
+
+//     const onIncrease = () => {
+//         if(number < 10) {
+//             setNumber(prevNumber => prevNumber + 1)
+//         }
+//         else {
+//             alert('10 is limit')
+//         }
+
+//     }
+//     const onDecrease = () => {
+//         if(number > 0) {
+//             setNumber(prevNumber => prevNumber - 1)
+//         }
+//         else {
+//             alert('0 is limit')
+//         }
+//     }
+//     return (
+//         <div>
+//             <h1 id="number">{number}</h1>
+//             <button onClick={onIncrease}>+1</button>
+//             <button onClick={onDecrease}>-1</button>
+//         </div>
+//     )
+// }
 
 export default Counter;
--- React/example/begin-react/src/index.js
+++ React/example/begin-react/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Counter from './Counter';
 
+// ReactDOM.render(
+//   // page에서 root인 것을 찾아서 <App />을 넣겠다.
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root') 
+//   // in public/index.html
+// );
 ReactDOM.render(
-  // page에서 root인 것을 찾아서 <App />을 넣겠다.
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root') 
-  // in public/index.html
+  <Counter />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
