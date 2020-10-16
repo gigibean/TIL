39d39796f7ceb18af7b683f6e3ad44a3da955a28
gigibean <gigibean@naver.com>
2020-10-16 11:04:20 +0900

    Update buttons

--- React/example/styling-with-sass/src/App.js
+++ React/example/styling-with-sass/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button>Button</Button>
+        <Button size="large">Button</Button>
+        <Button size="medium">Button</Button>
+        <Button size="small">Button</Button>
       </div>
     </div>
   );
--- React/example/styling-with-sass/src/components/Button.js
+++ React/example/styling-with-sass/src/components/Button.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Button.scss';
 
-function Button({children}) {
+function Button({children, size}) {
     return (
-        <button className="Button">{children}</button>
+        <button className={classNames('Button', size)}>{children}</button>
     );
 }
 
+Button.defaultProps = {
+    size: 'medium',
+}
+
+
 export default Button;
+
