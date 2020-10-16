d19418e66b17169d86d5c46cb0385159a9540872
gigibean <gigibean@naver.com>
2020-10-16 11:35:32 +0900

    Update colors mixin

--- React/example/styling-with-sass/src/App.js
+++ React/example/styling-with-sass/src/App.js
@@ -6,9 +6,19 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button size="large">Button</Button>
-        <Button size="medium">Button</Button>
-        <Button size="small">Button</Button>
+        <Button size="large" color="red">Button</Button>
+        <Button size="medium" color="grape">Button</Button>
+        <Button size="small" color="teal">Button</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="red">Button</Button>
+        <Button size="medium" color="grape">Button</Button>
+        <Button size="small" color="teal">Button</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="red">Button</Button>
+        <Button size="medium" color="grape">Button</Button>
+        <Button size="small" color="teal">Button</Button>
       </div>
     </div>
   );
--- React/example/styling-with-sass/src/components/Button.js
+++ React/example/styling-with-sass/src/components/Button.js
@@ -2,14 +2,15 @@ import React from 'react';
 import classNames from 'classnames';
 import './Button.scss';
 
-function Button({children, size}) {
+function Button({children, size, color}) {
     return (
-        <button className={classNames('Button', size)}>{children}</button>
+        <button className={classNames('Button', size, color)}>{children}</button>
     );
 }
 
 Button.defaultProps = {
     size: 'medium',
+    color: '#ffa8a8',
 }
 
 
