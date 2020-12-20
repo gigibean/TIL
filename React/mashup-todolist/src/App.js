2bdb015d53d8eed971015112503457738b708660
gigibean <gigibean@naver.com>
2020-12-20 17:36:30 +0900

    Update App.js

--- React/mashup-todolist/src/App.js
+++ React/mashup-todolist/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createGlobalStyle } from 'styled-components'
 import TodoTemplate from './components/TodoTemplate'
 import TodoHead from './components/TodoHead'
+import TodoList from './components/TodoList'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -15,6 +16,7 @@ function App() {
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
+        <TodoList />
       </TodoTemplate>
     </>
   )
