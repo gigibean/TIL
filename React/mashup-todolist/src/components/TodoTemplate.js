210445f429836c2857b5e97dc172b325e401fd87
gigibean <gigibean@naver.com>
2020-12-20 17:03:39 +0900

    Create TodoTemplate.js

---
+++ React/mashup-todolist/src/components/TodoTemplate.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TodoTemplateBlock = styled`
+    width: 512px;
+    height: 768px;
+
+    position: relative; /* 박스 하단에 버튼 위치 위해 */
+    background: white;
+    border-radius: 16px;
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+
+    margin: 0 auto;
+
+    margin-top: 96px;
+    margin-bottom: 32px;
+    display: flex;
+    flex-direction: column;
+`
+
+function TodoTemplate({ children }) {
+  return <TodoTemplateBlock>{children}</TodoTemplateBlock>
+}
+
+export default TodoTemplate
