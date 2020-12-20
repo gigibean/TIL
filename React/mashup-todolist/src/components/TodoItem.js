55257400562baa462866362e8dd51da689027da0
gigibean <gigibean@naver.com>
2020-12-21 04:41:53 +0900

    Update TodoItem.js
    onToggle, onRemove (dispatch)

--- React/mashup-todolist/src/components/TodoItem.js
+++ React/mashup-todolist/src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 import { MdDone, MdDelete } from 'react-icons/md'
+import { useTodoDispatch } from '../TodoContext'
 
 const Remove = styled.div`
   opacity: 0;
@@ -54,11 +55,17 @@ const TodoItemBlock = styled.div`
 `
 
 function TodoItem({ id, done, text }) {
+  const dispatch = useTodoDispatch()
+  const onToggle = () => dispatch({ type: 'TOGGLE', id })
+  const onRemove = () => dispatch({ type: 'REMOVE', id })
+
   return (
     <TodoItemBlock>
-      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <CheckCircle done={done} onClick={onToggle}>
+        {done && <MdDone />}
+      </CheckCircle>
       <Text done={done}>{text}</Text>
-      <Remove>
+      <Remove onClick={onRemove}>
         <MdDelete />
       </Remove>
     </TodoItemBlock>
