9f1e3dc73e7c360f29c0e84a122b9452317c9588
gigibean <gigibean@naver.com>
2020-12-21 04:29:54 +0900

    Update TodoList.js
    checking status and rendering components by useContext

--- React/mashup-todolist/src/components/TodoList.js
+++ React/mashup-todolist/src/components/TodoList.js
@@ -11,14 +11,18 @@ const TodoListBlock = styled.div`
 `
 
 function TodoList() {
-  const state = useTodoState()
-  console.log(state)
+  const todos = useTodoState()
+
   return (
     <TodoListBlock>
-      <TodoItem text="프로젝트 생성하기" done={true} />
-      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-      <TodoItem text="Context 만들기" done={false} />
-      <TodoItem text="기능 구현하기" done={false} />
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   )
 }
