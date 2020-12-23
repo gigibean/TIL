8b33861574e54928d51ca43aa067721c02e4d9ca
gigibean <gigibean@naver.com>
2020-12-23 22:58:03 +0900

    Create User.js & Get each User's info using axios

--- React/api-integrate/src/UsersReducer.js
+++ React/api-integrate/src/UsersReducer.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import axios from "axios";
 import useAsync from "./useAsync";
+import User from "./User";
 
 async function getUsers() {
   const response = await axios.get(
@@ -10,6 +11,7 @@ async function getUsers() {
 }
 
 function Users() {
+  const [userId, setUserId] = useState(null);
   const [state, refetch] = useAsync(getUsers, [], true);
 
   const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
@@ -21,12 +23,17 @@ function Users() {
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{ cursor: "pointer" }}
+          >
             {user.username}({user.name})
           </li>
         ))}
       </ul>
       <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
