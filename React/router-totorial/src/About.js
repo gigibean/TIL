f997b357964600f49c789c00cb497d02f1447ea3
gigibean <gigibean@naver.com>
2020-12-24 18:04:36 +0900

    Get Query

--- React/router-totorial/src/About.js
+++ React/router-totorial/src/About.js
@@ -1,10 +1,16 @@
 import React from "react";
+import qs from "qs";
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const detail = query.detail === "true";
   return (
     <div>
       <h1>About</h1>
       <p>this page is for introducing</p>
+      {detail && <p>추가적인 정보</p>}
     </div>
   );
 };
