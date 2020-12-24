859b08f84afd2899c1dddb174077680445d3940a
gigibean <gigibean@naver.com>
2020-12-24 15:57:18 +0900

    Creaete Home.js & About.js

---
+++ React/router-totorial/src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>this page is for introducing</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ React/router-totorial/src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>home</h1>
+      <p>this is the first page</p>
+    </div>
+  );
+};
+
+export default Home;
