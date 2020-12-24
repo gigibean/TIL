61be811ae8ed9696b81ac5c12214a96cdc40aac8
gigibean <gigibean@naver.com>
2020-12-24 16:08:19 +0900

    Update App.js
    Using Link

--- React/router-totorial/src/App.js
+++ React/router-totorial/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </div>
