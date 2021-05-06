2ac9044deb625b6190a001774256bb7558ff1bb9
gigibean <gigibean@naver.com>
2021-05-06 17:21:11 +0900

    Update 1676.js
    코드 리팩토링
    2deep loop -> 1 deep loop

--- DataStructureAndAlgorithm/algorithms/math/1676.js
+++ DataStructureAndAlgorithm/algorithms/math/1676.js
@@ -1,16 +1,21 @@
 const fs = require("fs");
+const { parse } = require("node:path");
 const input = parseInt(
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
     : `100`
 );
+// const input = parseInt(fs.readFileSync("/dev/stdin").toString());
 
 const getFiveMultiples = (x) => {
   let result = 0;
-  for (let i = 1; i <= x; i++) {
-    for (let j = Math.pow(5, i); j <= x; j += Math.pow(5, i)) {
-      result += 1;
-    }
+  //   for (let i = 1; i <= x; i++) {
+  //     for (let j = Math.pow(5, i); j <= x; j += Math.pow(5, i)) {
+  //       result += 1;
+  //     }
+  //   }
+  for (let i = 5; i <= x; i *= 5) {
+    result += parseInt(x / i);
   }
   return result;
 };
