ee5c195d95373d0a7e2a8c52eeb5ab6135afb6cb
gigibean <gigibean@naver.com>
2022-12-04 16:36:54 +0900

    Create 최솟값 만들기.js

---
+++ DataStructureAndAlgorithm/algorithms/programmers/최솟값 만들기.js
@@ -0,0 +1,8 @@
+function solution(A, B) {
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  return A.reduce((acc, cur, i) => acc + cur * B[i], 0);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12941
