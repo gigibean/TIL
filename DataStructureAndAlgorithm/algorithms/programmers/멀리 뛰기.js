5adc47cd2cb325d6c62d76cdcc568eeea4ddd0b6
gigibean <gigibean@naver.com>
2022-12-12 15:44:57 +0900

    Create 멀리 뛰기.js

---
+++ DataStructureAndAlgorithm/algorithms/programmers/멀리 뛰기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let pre = 0,
+    post = 1,
+    acc = 0;
+  for (let i = 2; i <= n + 1; i++) {
+    acc = (pre + post) % 1234567;
+    pre = post;
+    post = acc;
+  }
+  return acc;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12914
