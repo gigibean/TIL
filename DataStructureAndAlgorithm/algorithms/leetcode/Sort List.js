8b5db504c91d9be01c2565775de10d74262ed0d3
gigibean <gigibean@naver.com>
2022-12-23 19:53:41 +0900

    Update Sort List.js

--- DataStructureAndAlgorithm/algorithms/leetcode/Sort List.js
+++ DataStructureAndAlgorithm/algorithms/leetcode/Sort List.js
@@ -12,6 +12,53 @@
 var sortList = function (head) {
   const merge = (left, right) => {
     const list = new ListNode();
+    let tail = list;
+    while (left !== null && right !== null) {
+      if (left.val > right.val) {
+        tail.next = right;
+        right = right.next;
+      } else {
+        tail.next = left;
+        left = left.next;
+      }
+      tail = tail.next;
+    }
+    tail.next = left !== null ? left : right;
+    return list.next;
+  };
+  const getMid = (head) => {
+    let mid = null;
+    while (head !== null && head.next !== null) {
+      mid = mid !== null ? mid.next : head;
+      head = head.next.next;
+    }
+    const right = mid.next;
+    mid.next = null;
+    return right;
+  };
+  const mergeSort = (head) => {
+    if (head == null || head.next == null) return head;
+    const right = getMid(head);
+    const left = head;
+    return merge(mergeSort(left), mergeSort(right));
+  };
+  return mergeSort(head);
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+  const merge = (left, right) => {
+    const list = new ListNode();
     let tmp = list;
     while (left !== null && right !== null) {
       if (right.val < left.val) {
